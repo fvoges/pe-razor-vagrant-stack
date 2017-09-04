856600bc3551a322cbd800da1072360d3be29174
Federico Voges <federico@puppet.com>
2017-09-04 12:30:49 +0100

    update simlinks for PE 2016

--- puppet/misc/make_link_to_site_pp.pp
+++ puppet/misc/make_link_to_site_pp.pp
@@ -1,14 +1,14 @@
+$directories = [
+  '/etc/puppetlabs/code/environments',
+  '/etc/puppetlabs/code/environments/production',
+  '/etc/puppetlabs/code/environments/production/manifests',
+]
 
-file { [ '/etc/puppetlabs/puppet/environments', '/etc/puppetlabs/puppet/environments/production', '/etc/puppetlabs/puppet/environments/production/manifests' ] :
+file { $directories :
   ensure => directory,
 }
 
-file { '/etc/puppetlabs/puppet/environments/production/manifests/site.pp':  
-  ensure => link, 
-  target => '/tmp/puppet/manifests/site.pp',
-}
-
-file { '/etc/puppetlabs/puppet/manifests/site.pp':  
-  ensure => link, 
+file { '/etc/puppetlabs/code/environments/production/manifests/site.pp':
+  ensure => link,
   target => '/tmp/puppet/manifests/site.pp',
 }
