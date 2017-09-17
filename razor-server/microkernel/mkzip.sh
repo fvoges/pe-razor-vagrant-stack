8a24a8d43d702c6a1e0525143e5a0ab188436183
Federico Voges <federico@puppet.com>
2017-09-17 16:01:46 +0200

    the script now pulls the code from the puppet module
    no need to keep two copies of the code

--- razor-server/microkernel/mkzip.sh
+++ razor-server/microkernel/mkzip.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+if [ ! -d /vagrant ]
+then
+  echo "This script should be used from the razor-server Virtual Machine"
+  exit 1
+fi
+
+mkdir -p lib/ruby/facter
+cp /vagrant/puppet/modules/site/lib/facter/* lib/ruby/facter
 zip -r /etc/puppetlabs/razor-server/mk-extension.zip lib/
+
